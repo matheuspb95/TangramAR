b0a9efc1a905317aa8242bd7e684d6a778bcf072
matheuspb95 <matheuspb95@gmail.com>
2019-11-11 19:01:23 -0400

    inicio

---
+++ Assets/FadeAndLoadScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeAndLoadScene : MonoBehaviour
+{
+    public Image FadePanel;
+    public float fadeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string scene){
+        StartCoroutine(Fade(scene));
+    }
+
+    IEnumerator Fade(string scene){
+        float t = 0;
+        while(t < 1){
+            t += Time.fixedDeltaTime / fadeTime;
+            FadePanel.color = new Color(0,0,0,t);
+            yield return new WaitForFixedUpdate();
+        }
+        SceneManager.LoadScene(scene);
+    }
+}
