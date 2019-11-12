118f9cf50aeb3d271baca80a71585593f65a460d
Matheus Palheta <matheuspb95@gmail.com>
2019-11-12 14:46:20 -0400

    Novas imagens

--- Assets/ChangePanel.cs
+++ Assets/ChangePanel.cs
@@ -9,7 +9,7 @@ public class ChangePanel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.SetInt("Openned", 1);
     }
 
     // Update is called once per frame
---
+++ Assets/LoadTutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {        
+        int value = PlayerPrefs.GetInt("Openned");
+        Debug.Log(value);
+        if(value <= 0){
+            SceneManager.LoadScene("Tutorial");
+        }else{            
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
